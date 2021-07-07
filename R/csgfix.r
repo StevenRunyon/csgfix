c21fb99d9a75361abc2b3c6313ac2f5665f1a217
Steven Runyon <48095299+StevenRunyon@users.noreply.github.com>
2021-07-07 13:24:26 -0400

    several updates
    removed functions that don't do anything yet

--- R/csgfix.r
+++ R/csgfix.r
@@ -4,46 +4,47 @@
 
 #'changes data to Movebank format for use with ctmm package
 #' @param data: dataframe, the input data
-#' @param projectionType: boolean or character, FALSE if you don't want to convert lat/long to X/Y (if you want to use an existing X and Y). Accepted values: "North_America_Equidistant_Conic"
+#' @param projectionType: character or function, NULL if you don't want to convert lat/long to X/Y (if you want to use an existing X and Y). Otherwise a character (the name of a projection), or a function(lat, long, datetime) where lat and long are vectors of numbers and datetime is a vector of POSIXct corresponding to the time of those observations. should return a data frame with two numeric columns, x and y
 #' @param timeformat: character (format for use in as.POSIXct), the format Date_Time is in
 #' @param ct: boolean, FALSE if you want time to be saved as a string in move bank format, TRUE if you want it as a POSIXct
-#' @param coordFix: function, a function that converts lat and long at datetime to NAD83, inputs are vectors lat (numeric), long (numeric), and datetime (POSIXct). returns a data frame with columns "lat" and "long". FALSE if you don't want to change the coords at all.
-bank <- function(data, projectionType="na", timeformat="%m/%d/%Y %T", ct=TRUE, coordFix=function(lat,long,n){return(data.frame(lat,long))} ){
+#' @param tonad83: boolean, TRUE if you want to convert from WGS84 to NAD83 before projection
+bank <- function(data, projectionType="na", timeformat="%m/%d/%Y %T", ct=TRUE, tonad83=FALSE){
 
   #adds "timestamp" column
   if(ct){
-    timeline <- as.POSIXct(data$Date_Time, format="%m/%d/%Y %T", tz="GMT")
+    timeline <- as.POSIXct(data$Date_Time, format=timeformat, tz="GMT")
     data$timestamp <- timeline
   }
   else{
-    timeline <- as.POSIXlt(data$Date_Time, format="%m/%d/%Y %T", tz="GMT")
+    timeline <- as.POSIXlt(data$Date_Time, format=timeformat, tz="GMT")
     data$timestamp <- paste0((1900+timeline$year),"-",pad(timeline$mon,2),"-",pad(timeline$mday,2)," ",pad(timeline$hour,2),":",pad(timeline$min,2),":",pad(timeline$sec,2),".000")
   }
 
-  #projects coordinates if using north america equidistant conic
-  if(tolower(projectionType)%in%c("north_america_equidistant_conic", "na", "north america equidistant conic", "naec")){
-    res <- coordFix(data$Latitude, data$Longitude, timeline) #gets coordinates in NAD83 as "res"
-    res2 <- projectionNA(res$lat, res$long) #gets X and Y coordinates as "res"
-    data$x <- res2$x
-    data$y <- res2$y
+  #projects coordinates
+  if(!is.null(projectionType)){
+    if(projectionType==FALSE)
+      res <- data.frame(y=data$Latitude, x=data$Longitude)
+    else if(typeof(projectionType)%in%c("closure","special","builtin"))
+      res <- projectionType(data$Latitude, data$Longitude, timeline)
+    else{
+      projectionType <- tolower(projectionType)
+      if(tonad83)
+        outType <- "NAD83"
+      else
+        outType <- "WGS84"
+      if(projectionType%in%c("north_america_equidistant_conic", "na", "north america equidistant conic", "naec")){
+        #res <- coordFix(data$Latitude, data$Longitude, timeline) #gets coordinates in NAD83 as "res"
+        res <- fixCoords(data$Latitude, data$Longitude, "WGS84", outType)
+        res <- projectionNA(res$lat, res$long) #gets X and Y coordinates as "res"
+      }
+    }
+    data$x <- res$x
+    data$y <- res$y
   }
 
   if("GMT_offset"%in%colnames(data))
     data$study.timezone <- paste0("Etc/GMT",c("","+","+")[sign(data$GMT_offset)+2],data$GMT_offset) #adds a study.timezone column, with the time zone given as "Etc/GMT-9" for example
   data$sensor.type <- "gps" #adds a character "gps" called sensor.type
-
-  ##removed local timestamp for now, timezones don't work properly
-  #if("LocalTime"%in%colnames(data)){ #if there exists a "LocalTime" column
-  #  if(ct){
-  #    timeline <- as.POSIXct(data$LocalTime, format="%m/%d/%Y %T", tz=data$study.timezone)
-  #    data$study.local.timestamp <- timeline
-  #  }
-  #  else{
-  #    timeline <- as.POSIXlt(data$LocalTime, format="%m/%d/%Y %T", tz=data$study.timezone)
-  #    data$study.local.timestamp <- paste0((1900+timeline$year),"-",pad(timeline$mon,2),"-",pad(timeline$mday,2)," ",pad(timeline$hour,2),":",pad(timeline$min,2),":",pad(timeline$sec,2),".000")
-  #  }
-  #  #adds "study.local.timestamp" column (same way as timestamp)
-  #}
 
   #adds ASY column if there isn't already one
   if(!"ASY"%in%colnames(data) && all(c("Animal_ID","Season","SeasonYr")%in%colnames(data)))
@@ -52,12 +53,21 @@ bank <- function(data, projectionType="na", timeformat="%m/%d/%Y %T", ct=TRUE, c
   #renames columns
   oldcolnames <- c("Animal_ID","Latitude","Longitude","KPH","Heading","Altitude","HDOP")
   newcolnames <- c("individual.local.identifier","location.lat","location.long","ground.speed","heading","height.raw","gps.hdop")
-  colnames(data)[match(oldcolnames,colnames(data))] <- newcolnames
+  #colnames(data)[match(oldcolnames,colnames(data))] <- newcolnames
+   for(x in 1:length(oldcolnames)){
+     y <- grep(oldcolnames[x], colnames(data))
+     if(length(y)!=0)
+       colnames(data)[colnames(data)==oldcolnames[x]] <- newcolnames[x]
+   }
 
   #limits data to necessary columns
-  data <- data[c("ASY", "x", "y",
-                   "timestamp", "individual.local.identifier", "study.timezone", "location.lat", "location.long",
-                   "ground.speed", "heading", "sensor.type", "gps.hdop")] #, "study.local.timestamp"
+  realcolnames <- c("ASY", "x", "y", "timestamp", "individual.local.identifier", "study.timezone", "location.lat", "location.long", "ground.speed", "heading", "sensor.type", "gps.hdop", "study.local.timestamp")
+  y <- c() #y will be a vector that tracks the column indecies we want to keep
+  for(x in 1:ncol(data)){
+    if(colnames(data)[x]%in%realcolnames)
+      y <- append(y, x)
+  }
+  data <- data[y] #limits the columns to the ones we want to keep
 
   return(data)
 }
@@ -65,14 +75,16 @@ bank <- function(data, projectionType="na", timeformat="%m/%d/%Y %T", ct=TRUE, c
 #'projects lat and long coordinates to x and y with equidistant conic (simple conic) projection
 #' @param lat: numeric vector, latitude (lat and long should be the same length)
 #' @param long: numeric vector, longitude
-#' @param type: boolean or character, FALSE to use your own parameters, or the name of a preset
+#' @param type: character, NULL to use your own parameters, or the name of a preset
 #' @param reflat, reflong, sp1, sp2: numeric, parameters for the projection (reflat and reflong are reference latitude and longitude (origin), sp1 and sp2 are standard parallels)
-projectionEquidistantConic <- function(lat, long, type=FALSE, reflat=0, reflong=0, sp1=20, sp2=60){
-  if(type=="North_America_Equidistant_Conic"){ #if you want this to be equivalent to ArcGIS, it must use NAD83 here
-    reflat <- 40
-    reflong <- -96
-    sp1 <- 20
-    sp2 <- 60
+projectionEquidistantConic <- function(lat, long, type=NULL, reflat=0, reflong=0, sp1=20, sp2=60){
+  if(!is.null(type)){
+    if(type=="North_America_Equidistant_Conic"){ #if you want this to be equivalent to ArcGIS, it must use NAD83 here
+      reflat <- 40
+      reflong <- -96
+      sp1 <- 20
+      sp2 <- 60
+    }
   }
   nn <- (cos(sp1)-cos(sp2))/(sp2-sp1)
   G <- cos(sp1)/nn + sp1
@@ -91,35 +103,38 @@ projectionEquidistantConic <- function(lat, long, type=FALSE, reflat=0, reflong=
 #'projects lat and long coordinates to x and y with North American equidistant conic projection
 #' @param lat: numeric vector, latitude (lat and long should be the same length)
 #' @param long: numeric vector, longitude
-projectionNA <- function(lat, long){
+projectionNA <- function(lat, long, v2=NULL){
   return(projectionEquidistantConic(lat, long, "North_America_Equidistant_Conic"))
 }
 
-#'converts coordinates from inType to outType
-#' @param lat: numeric vector, latitude (lat, long, and datetime should be the same length)
+#'projects lat and long coordinates to x and y with cylindrical equal area projection
+#' @param lat: numeric vector, latitude (lat and long should be the same length)
 #' @param long: numeric vector, longitude
-#' @param datetime: POSIXct vector, the datetime when lat and long were recorded
-#' @param inType: character, format that lat and long are in
-#' @param outType: character, format that you want the coordinates converted to
-fixCoords <- function(lat, long, datetime=FALSE, inType="WGS84", outType="NAD83"){
-  #absolutely a work in progresss
-
-  if(inType==outType)
-    return(data.frame(lat=lat, long=long))
-
-  #packages tried: htdp, gdalUtils
-  # if i have to port over FORTRAN code...
-  if(inType%in%c("WGS84","ITRF2000") && outType=="NAD83"){ # ITRF2000 (WHS84 (G1150)) to NAD83 (CORS96)
-    require(sp)
-    require(rgdal)
-    coords <- data.frame(lat, long)
-    coordinates(coords) <- c("long","lat") #adds coordinates to coords
-    proj4string(coords) <- CRS("+init=epsg:4326 +ellps=WGS84 +datum=WGS84 +proj=longlat") #adds WGS84 CRS to coordinates
-    coords <- spTransform(coords, CRS("+init=epsg:4269 +ellps=GRS80 +datum=NAD83")) #transforms to NAD83
-    return(data.frame(lat=coords$lat, long=coords$long))
+#' @param type: character, NULL to use your own parameters, or the name of a preset ("lambert","behrmann","gall",...)
+#' @param standardlong, standardlat: numeric, parameters for the projection
+projectionCylindricalEqualArea <- function(lat, long, type=NULL, standardlat=0, standardlong=0){
+  if(!is.null(type) && typeof(type)=="character"){
+    type <- tolower(type)
+    if(type%in%c("lambert","l","0"))
+      standardlat <- 0
+    else if(type%in%c("behrmann","behrman","b","30"))
+      standardlat <- pi/6
+    else if(type%in%c("smyth","smyth equal-surface","craster","craster rectangular","2:1","1:2","2"))
+      standardlat <- arccos(2/pi)
+    else if(type%in%c("edwards","trystan edwards","te","e"))
+      standardlat <- 37.24*(pi/180)
+    else if(type%in%c("hobo-dyer","dyer","hobo–dyer","hd","d"))
+      standardlat <- 37.30*(pi/180)
+    else if(type%in%c("gall-peters","gall","gall–peters","gall orthographic","peters","gp","g","45"))
+      standardlat <- pi/4
+    else if(type%in%c("balthasart","m","50"))
+      standardlat <- 5*pi/18
+    else if(type%in%c("tobler","square","world in a square","tobler's world in a square","1:1","1"))
+      standardlat <- arccos(1/pi)
   }
-
-  return(data.frame(lat=lat, long=long))
+  x <- (long-standardlong)*cos(standardlat)
+  y <- sin(lat)/cos(standardlat)
+  return(data.frame(x, y))
 }
 
 #'pads out an integer x to width with zeroes (to the left)
@@ -137,9 +152,10 @@ pad <- function(x, width=3){
 #' @param timestep: numeric, the number of seconds between each observation
 #' @param timestart: POSIXct, the time you want the first observation to be
 #' @param multi: numeric, the distance the bird travels in each observation (in change in lat/long) will be a random number in [0,multi)
+#' @param rand: function, a function that generates random numbers in [0,1], where the first parameter is the number of numbers to generate
 #' @param fakemisc: boolean, if you want to fake the other columns needed for bank()
-#' @param n: numeric, the Animal_ID or FALSE to randomize it
-generateRandomFlight <- function(origin=c(0,0), names="csg", obs=1000, maxturn=15, timestep=3600, timestart=Sys.time(), multi=1/500, fakemisc=TRUE, n=FALSE){
+#' @param n: numeric, the Animal_ID or NULL to randomize it
+generateRandomFlight <- function(origin=c(0,0), names="csg", obs=1000, maxturn=15, timestep=3600, timestart=Sys.time(), multi=1/500, rand=stupidRnorm, fakemisc=TRUE, n=NULL){
   #work in progress
   names <- tolower(names)
    lat <- c(origin[1])
@@ -149,11 +165,11 @@ generateRandomFlight <- function(origin=c(0,0), names="csg", obs=1000, maxturn=1
   x <- 0
   while(x<(obs-1)){
     x <- x + 1
-    dist <- stupidRnorm(1)*multi
+    dist <- rand(1)*multi
       oh <- (head[x]-360*as.numeric(head[x]>180))*pi/180
      lat <- append( lat,  lat[x]+dist*sin(oh))
     long <- append(long, long[x]+dist*cos(oh))
-    head <- append(head, (head[x]+(stupidRnorm(1)-0.5)*maxturn*2)%%360)
+    head <- append(head, (head[x]+(rand(1)-0.5)*maxturn*2)%%360)
     time <- append(time, time[x]+timestep)
   }
   data <- data.frame(lat, long, head, time)
@@ -164,14 +180,14 @@ generateRandomFlight <- function(origin=c(0,0), names="csg", obs=1000, maxturn=1
   if(tolower(names)%in%c("movebank","mb","move"))
     colnames(data) <- c("location.lat", "location.long", "heading", "timestamp")
   if(fakemisc){
-    if(n==FALSE)
+    if(is.null(n))
       n <- floor(runif(1,min=1,max=1000))
-      data$Animal_ID <- n
-      data$ASY <- paste0(n, "Random", format(time[1], "%Y"))
-      data$KPH <- 0
-      data$Altitude <- 0
-      data$HDOP <- 0 #0 temporarily, will make these something better later
-      data$GMT_offset <- 0
+    data$Animal_ID <- n
+    data$ASY <- paste0(n, "Random", format(time[1], "%Y"))
+    data$KPH <- 0
+    data$Altitude <- 0
+    data$HDOP <- 0 #0 temporarily, will make these something better later
+    data$GMT_offset <- 0
   }
   return(data)
 }
@@ -189,8 +205,54 @@ stupidRnorm <- function(n, mean=0.5, sd=0.125){
 #' @param data: dataframe, the input data for bank(data)
 #' @param proj: CRS object, sent to move::move
 #' @param removeDuplicatedTimestamps: boolean, sent to move::move
-quickMove <- function(data, proj=CRS("+init=epsg:4326 +proj=longlat +ellps=WGS84 +datum=WGS84"), removeDuplicatedTimestamps=FALSE){
+quickMove <- function(data, proj=CRS("+init=epsg:4326 +ellps=WGS84 +datum=WGS84 +no_defs +proj=eqdc +lat_1=20 +lat_2=60 +lat_0=0 +lon_0=0"), removeDuplicatedTimestamps=FALSE){
   require(move)
-  b <- bank(data, coordFix = fixCoords)
+  b <- bank(data)
   return(move(x=b$x, y=b$y, time=b$timestamp, proj=proj, data=b, animal=b$individual.local.identifier, sensor=b$sensor, removeDuplicatedTimestamps=removeDuplicatedTimestamps))
+}
+
+quickATM <- function(data){
+  b <- bank(data, projectionType=NULL)
+  wgs84 <- "+init=epsg:4326 +ellps=WGS84 +datum=WGS84 +no_defs"
+  nad83 <- "+init=epsg:4269 +ellps=GRS80 +datum=NAD83 +no_defs"
+   eqdc <- "+proj=eqdc +lat_1=20 +lat_2=60 +lat_0=0 +lon_0=0"
+  require(sp)
+  require(ctmm)
+  x <- as.telemetry(b, datum=paste(wgs84, "+proj=latlong"), projection=paste(wgs84,eqdc))
+  return(x)
+}
+
+#'converts coordinates from inType to outType
+#' @param lat: numeric vector, latitude (lat, long, and datetime should be the same length)
+#' @param long: numeric vector, longitude
+#' @param inType: character, format that lat and long are in
+#' @param outType: character, format that you want the coordinates converted to
+#' @param datetime: POSIXct vector, the datetime when lat and long were recorded (currently unused)
+fixCoords <- function(lat, long, inType="WGS84", outType="NAD83", datetime=NULL){
+  #absolutely a work in progresss
+
+  coordf <- data.frame(lat=lat, long=long)
+
+  if(inType==outType)
+    return(coordf)
+
+  require(sf)
+
+  inType <- tolower(inType)
+  outType <- tolower(outType)
+
+  #creates a sf object with the existing crs
+  if(inType%in%c("wgs84","itrf2000","wgs","w","g1150"))
+    coordf <- st_as_sf(coordf, coords=c("long","lat"), crs=4326)
+  else if(inType%in%c("nad83","nad","n","cors96","nad83 (cors96)"))
+    coordf <- st_as_sf(coordf, coords=c("long","lat"), crs=4269)
+
+  #converts datum?
+  if(outType%in%c("nad83","nad","n","cors96","nad83 (cors96)")) # ITRF2000 (WHS84 (G1150)) to NAD83 (CORS96)
+    coordf <- st_transform(coordf, crs=4269, type="datum")
+  else if(outType%in%c("wgs84","itrf2000","wgs","w","g1150"))
+    coordf <- st_transform(coordf, crs=4326, type="datum")
+
+  coordf <- st_coordinates(coordf) #changes geometry points to data frame of numeric columns lat and long, then returns it
+  return(data.frame(lat=coordf[,2], long=coordf[,1]))
 }
